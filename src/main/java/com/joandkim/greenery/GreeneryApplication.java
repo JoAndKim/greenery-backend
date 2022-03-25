21220d335c7d732016fde569d0f96694686b6e8f
kyupid <iamscvv@gmail.com>
2022-03-26 01:22:14 +0900

    프로젝트 초기화

---
+++ src/main/java/com/joandkim/greenery/GreeneryApplication.java
@@ -0,0 +1,13 @@
+package com.joandkim.greenery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreeneryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreeneryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joandkim/greenery/GreeneryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joandkim.greenery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreeneryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
