546670be13ca10acadd526d11e2bb08f19c3a275
kyupid <iamscvv@gmail.com>
2022-04-12 21:24:22 +0900

    #9 delete all constraints between tables
    - faster implement

--- src/main/resources/query.sql
+++ src/main/resources/query.sql
@@ -1,12 +1,10 @@
-SET foreign_key_checks = 0;
-DROP TABLE IF EXISTS comment CASCADE;
-DROP TABLE IF EXISTS member CASCADE;
-DROP TABLE IF EXISTS post CASCADE;
-DROP TABLE IF EXISTS like_number CASCADE;
-DROP TABLE IF EXISTS post_content CASCADE;
-SET foreign_key_checks = 1;
+DROP TABLE IF EXISTS `greenery`.comment;
+DROP TABLE IF EXISTS `greenery`.member;
+DROP TABLE IF EXISTS `greenery`.post;
+DROP TABLE IF EXISTS `greenery`.like_number;
+DROP TABLE IF EXISTS `greenery`.post_content;
 
-create table comment
+create table `greenery`.comment
 (
     id        bigint                              not null
         primary key,
@@ -16,17 +14,38 @@ create table comment
     reg_date  timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
     member_id bigint                              null,
     post_id   bigint                              null
-
 );
 
-create index fk_comment_author
-    on comment (member_id);
-
-create index fk_comment
-    on comment (post_id);
 
 
-create table member
+create table `greenery`.post
+(
+    id           bigint                              not null primary key,
+    hits         int                                 null,
+    like_numbers int                                 null,
+    reg_date     timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
+    title        varchar(255)                        null,
+    member_id    bigint                              null
+);
+
+create table `greenery`.like_number
+(
+    id        bigint not null
+        primary key,
+    member_id bigint null,
+    post_id   bigint null
+);
+
+create table `greenery`.post_content
+(
+    id             bigint       not null
+        primary key,
+    content        longtext     null,
+    post_image_url varchar(255) null,
+    post_id        bigint       null
+);
+
+create table `greenery`.member
 (
     id                bigint       not null
         primary key,
@@ -35,44 +54,3 @@ create table member
     profile_image_url varchar(255) null,
     username          varchar(255) null
 );
-
-create table post
-(
-    id           bigint                              not null primary key,
-    hits         int                                 null,
-    like_numbers int                                 null,
-    reg_date     timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
-    title        varchar(255)                        null,
-    member_id    bigint                              null,
-    constraint fk_post_author
-        foreign key (member_id) references member (id)
-);
-
-create table like_number
-(
-    id        bigint not null
-        primary key,
-    member_id bigint null,
-    post_id   bigint null,
-    constraint fk_like_member
-        foreign key (member_id) references member (id),
-    constraint fk_like_post
-        foreign key (post_id) references post (id)
-);
-
-create table post_content
-(
-    id             bigint       not null
-        primary key,
-    content        longtext null,
-    post_image_url varchar(255) null,
-    post_id        bigint       null,
-    constraint fk_post_content
-        foreign key (post_id) references post (id)
-);
-
-alter table comment
-    add constraint fk_comment
-        foreign key (post_id) references post (id),
-    add constraint fk_comment_author
-        foreign key (member_id) references member (id)
