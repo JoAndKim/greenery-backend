62341f89356fe73e912b4fcc811981d7800bd22b
kyupid <iamscvv@gmail.com>
2022-04-12 21:12:06 +0900

    #9 delete all dtos
    - re-create be better

--- src/main/java/com/joandkim/greenery/service/PostService.java
+++ src/main/java/com/joandkim/greenery/service/PostService.java
@@ -1,12 +1,8 @@
 package com.joandkim.greenery.service;
 
-import com.joandkim.greenery.dto.post.PostThumbnailResponse;
 import com.joandkim.greenery.mapper.PostMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,9 +10,4 @@ public class PostService {
 
     private final PostMapper postMapper;
 
-    public List<PostThumbnailResponse> getPosts() {
-        return postMapper.findAll().stream()
-                .map(PostThumbnailResponse::from)
-                .collect(Collectors.toList());
-    }
 }
