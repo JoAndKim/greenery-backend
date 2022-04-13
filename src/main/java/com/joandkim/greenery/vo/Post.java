469adea35d36e227796eb611221c8554fb33db32
kyupid <iamscvv@gmail.com>
2022-04-13 13:13:59 +0900

    #3 fix VO Post to map the table as it is

--- src/main/java/com/joandkim/greenery/vo/Post.java
+++ src/main/java/com/joandkim/greenery/vo/Post.java
@@ -3,16 +3,13 @@ package com.joandkim.greenery.vo;
 import lombok.Data;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 public class Post {
     private Long id;
-    private Member member;
     private String title;
-    private List<PostContent> postContents;
-    private List<Comment> comments;
     private LocalDateTime regDate;
     private Integer likeNumbers;
     private Integer hits;
+    private Long memberId;
 }
