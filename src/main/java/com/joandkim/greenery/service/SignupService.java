d65687345f4fe7b4f6415c88e6fda98e256aad33
kyupid <iamscvv@gmail.com>
2022-04-16 23:11:09 +0900

    #19 add encoding password when signing up

--- src/main/java/com/joandkim/greenery/service/SignupService.java
+++ src/main/java/com/joandkim/greenery/service/SignupService.java
@@ -3,6 +3,7 @@ package com.joandkim.greenery.service;
 import com.joandkim.greenery.mapper.MemberMapper;
 import com.joandkim.greenery.vo.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +13,8 @@ public class SignupService {
     private final MemberMapper memberMapper;
 
     public void signup(Member signupMember) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        signupMember.setPassword(encoder.encode(signupMember.getPassword()));
         memberMapper.save(signupMember);
     }
 }
