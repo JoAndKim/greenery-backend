e998eaf588eddb2e3cb41c76f1158020d16f3513
kyupid <iamscvv@gmail.com>
2022-04-29 01:27:37 +0900

    #6 impl uploading images API to S3

--- src/main/java/com/joandkim/greenery/controller/UploadController.java
+++ src/main/java/com/joandkim/greenery/controller/UploadController.java
@@ -1,6 +1,8 @@
 package com.joandkim.greenery.controller;
 
 import com.joandkim.greenery.dto.Image;
+import com.joandkim.greenery.util.s3.S3Uploader;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -9,14 +11,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @RestController
+@RequiredArgsConstructor
 public class UploadController {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final S3Uploader s3Uploader;
 
     @PostMapping("/api/image")
-    public ResponseEntity<Image> image(@RequestParam("imageFile") MultipartFile multipartFile) {
+    public ResponseEntity<Image> image(@RequestParam("imageFile") MultipartFile multipartFile) throws IOException {
         logger.info("multipartFile: {}", multipartFile);
+        s3Uploader.upload(multipartFile, "static");
         return ResponseEntity.ok(new Image());
     }
 }
