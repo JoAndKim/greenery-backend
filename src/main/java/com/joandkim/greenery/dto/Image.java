71ca445abf4bca75c81ab64c503d1258ff7eb175
kyupid <iamscvv@gmail.com>
2022-04-29 00:03:34 +0900

    #6 rename property of contentImageUrl

--- src/main/java/com/joandkim/greenery/dto/Image.java
+++ src/main/java/com/joandkim/greenery/dto/Image.java
@@ -4,5 +4,5 @@ import lombok.Getter;
 
 @Getter
 public class Image {
-    private final String contentImageUrl = "https://mblogthumb-phinf.pstatic.net/MjAxOTA1MTlfMTI4/MDAxNTU4MTk1OTc5NzY3.sXSCMmuLIcvUSRTzjuX1LpffB8yqzrdRr3sTzm7Urfkg.p20o7lNofvQXmYEVuZCzYiAouUJtWR122Ol72NtlsYwg.JPEG.professionaldog/SE-ed5a75cb-27a1-4411-bfdb-f4d04a793ea0.jpg?type=w800";
+    private final String imageUrl = "https://mblogthumb-phinf.pstatic.net/MjAxOTA1MTlfMTI4/MDAxNTU4MTk1OTc5NzY3.sXSCMmuLIcvUSRTzjuX1LpffB8yqzrdRr3sTzm7Urfkg.p20o7lNofvQXmYEVuZCzYiAouUJtWR122Ol72NtlsYwg.JPEG.professionaldog/SE-ed5a75cb-27a1-4411-bfdb-f4d04a793ea0.jpg?type=w800";
 }
