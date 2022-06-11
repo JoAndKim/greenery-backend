c70973d97fbcd65759ed8d1a77272fc2c1955916
kyupid <iamscvv@gmail.com>
2022-06-11 13:16:52 +0900

    #56 fix API mapping URL

--- src/main/java/com/joandkim/greenery/controller/post/PostController.java
+++ src/main/java/com/joandkim/greenery/controller/post/PostController.java
@@ -70,7 +70,7 @@ public class PostController {
         return ResponseEntity.ok(posts);
     }
 
-    @GetMapping("/{userId}/my")
+    @GetMapping("/{userId}/me")
     public ResponseEntity<Posts> getMyPosts(@PathVariable Long userId) {
         Posts posts = postService.getMyPosts(userId);
         return ResponseEntity.ok(posts);
